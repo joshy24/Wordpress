65ae0e63cadf68947a33c2eb11ec7d341c9f9473
Joshua <joshuamajebi@yahoo.com>
2017-11-25 12:03:15 +0100

    setup short codes for displaying recent 5 posts under search bar

---
+++ wp-content/themes/unite-child/films.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+Template Name: Films Page
+*/
+?>
+
+div id="primary" class="content-area col-sm-12 col-md-8 <?php echo of_get_option( 'site_layout' ); ?>">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+                
+                //Returns Array of Term for "my_taxonomy"
+                $terms = get_the_terms( get_the_ID(), 'genre' );
+
+                if ( $terms && ! is_wp_error( $terms ) ) : 
+
+                    $terms_links = array();
+
+                    foreach ( $terms as $term ) {
+                        $terms_links[] = $term->name;
+                    }
+
+                    $on_terms = join( ", ", $terms_links );
+                    ?>
+
+                    <p>
+                        <?php printf( esc_html__( 'Categories : <span>%s</span>', 'textdomain' ), esc_html( $on_terms ) ); ?>
+                    </p>
+                <?php endif; ?>
+            
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
